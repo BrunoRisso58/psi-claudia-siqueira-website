d6bf03ad165a6e721d852f0733d845de1bbf1a43
Bruno Risso <brunorisso58@gmail.com>
2025-07-25 18:01:39 -0300

    Finalização

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener('scroll', function () {
     const header = document.getElementById('nav-bar');
     if (window.scrollY > 50) {
-        console.log('JS!!!');
         header.classList.add('shrink');
     } else {
         header.classList.remove('shrink');
